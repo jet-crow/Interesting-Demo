76e429d049065c777c8c148cfb1a520981951c46
jet-crow <1298874688@qq.com>
2024-06-17 15:26:20 +0800

    feat:naive-ui

--- vue3-demo/nuxt.config.ts
+++ vue3-demo/nuxt.config.ts
@@ -1,5 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@vant/nuxt', '@nuxtjs/tailwindcss']
+  modules: [
+    '@vant/nuxt',
+    '@nuxtjs/tailwindcss',
+    "nuxtjs-naive-ui"
+  ],
+  vite: {
+    plugins: [
+      AutoImport({
+        imports: [
+          {
+            'naive-ui': [
+              'useDialog',
+              'useMessage',
+              'useNotification',
+              'useLoadingBar'
+            ]
+          }
+        ]
+      }),
+      Components({
+        resolvers: [NaiveUiResolver()]
+      })
+    ]
+  }
 })
